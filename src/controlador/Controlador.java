3a9aae9dcd45ffc9a5bb7701a8a360066b4b3f77
David Alejandro Marin Alzate <jandro240@gmail.com>
2017-04-05 11:33:14 -0500

    Atomata a partir de una hilera

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -5,21 +5,49 @@
  */
 package controlador;
 
+import Modelo.AFDeterministico;
+import Modelo.AFNoDeterministico;
 import java.util.ArrayList;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.HashMap;
 
 /**
  *
  * @author alejandro
  */
 public class Controlador {
+    
+    private AFDeterministico afd;
+    private AFNoDeterministico afnd;
 
     public Controlador() {
         
     }
     
     public void construirAtomata(String stringAutomata)
+    {
+        ArrayList<String> componentesAF = obtenerCompontesAtomata(stringAutomata);
+        
+        if (!componentesAF.isEmpty()) 
+        {
+            HashMap estados = obtenerEstados(componentesAF.get(0));
+            HashMap lenguaje =obtenerLenguaje(componentesAF.get(1));
+            HashMap estadosIniciales =obtenerEstadosIniciales(componentesAF.get(3));
+            HashMap estadosAceptacion = obtenerEstadosAceptacion(componentesAF.get(4));
+            ArrayList[][] transiciones = obtenerTransiciones(componentesAF.get(2),estados,lenguaje);
+            
+        }
+        else
+        {
+            error();
+        }
+    }
+    
+    private void error()
+    {
+        System.out.println("ERROR");
+    }
+    
+    private ArrayList obtenerCompontesAtomata(String stringAutomata)
     {
         ArrayList<String> automata = new ArrayList<>();
         //elimimando los espacios
@@ -31,6 +59,7 @@ public class Controlador {
             while (i < size) {                
                 if(stringAutomata.charAt(i) == '[')
                 {
+                    i++;
                     String s = "";
                     while (stringAutomata.charAt(i) != ']') {
                         s = s.concat(String.valueOf(stringAutomata.charAt(i)));
@@ -45,11 +74,132 @@ public class Controlador {
         {
             error();
         }
+        
+        return automata;
     }
-    
-    private void error()
+
+    private HashMap obtenerEstados(String strEstados) 
     {
-        System.out.println("ERROR");
+        return subStringComa(strEstados);  
+    }
+
+    private HashMap obtenerLenguaje(String strLenguaje) 
+    {
+        return subStringComa(strLenguaje);
+    }
+
+    
+
+    private HashMap obtenerEstadosIniciales(String strEstadosIniciales) 
+    {
+         return subStringComa(strEstadosIniciales);
+    }
+
+    private HashMap obtenerEstadosAceptacion(String strEstadosAceptacion) {
+        return subStringComa(strEstadosAceptacion);
     }
     
+    private HashMap subStringComa(String hilera)
+    {
+        HashMap<String,Integer> retorno = new HashMap<>();
+        int size = hilera.length();
+        
+        int i = 0;
+        int j = 0;
+        while (i < size) 
+        {                
+            String s = "";
+            while (i< size && hilera.charAt(i) != ',') 
+            {
+                s = s.concat(String.valueOf(hilera.charAt(i)));
+                i++;
+            }
+                retorno.put(s, j);
+                i++;
+                j++;
+        }
+      
+        return retorno;
+    }
+    
+    private ArrayList<String> subStringComa(String hilera,int a)
+    {
+        ArrayList<String> retorno = new ArrayList<>();
+        int size = hilera.length();
+        
+        int i = 0;
+        int j = 0;
+        while (i < size) 
+        {                
+            String s = "";
+            while (i< size && hilera.charAt(i) != ',') 
+            {
+                s = s.concat(String.valueOf(hilera.charAt(i)));
+                i++;
+            }
+                retorno.add(s);
+                i++;
+                j++;
+        }
+      
+        return retorno;
+    }
+
+    private ArrayList[][] obtenerTransiciones(String hilera, HashMap<String,Integer> estados, HashMap<String,Integer> lenguaje) 
+    {
+        ArrayList<String>[][] matriz = new ArrayList[estados.size()][lenguaje.size()];
+        ArrayList<String> transiciones = splitTransiciones(hilera);
+        for (int i = 0; i < transiciones.size(); i++) {
+            ArrayList<String> transicionArrayList = subStringComa(transiciones.get(i),0);
+            
+            String estadoActual = transicionArrayList.get(0);
+            String estadoSiguiente = transicionArrayList.get(2);
+            String simbolo = transicionArrayList.get(1);
+            
+            int fila = estados.get(estadoActual);
+            int columna = lenguaje.get(simbolo);
+            
+            ArrayList<String> posicionArrayList = matriz[fila][columna];
+            if (posicionArrayList != null) 
+            {
+                posicionArrayList.add(estadoSiguiente);
+            }
+            else
+            {
+                posicionArrayList = new ArrayList<>();
+                posicionArrayList.add(estadoSiguiente);
+            }
+            
+            matriz[fila][columna] = posicionArrayList;            
+        }
+        
+        
+        return matriz;
+    }
+
+    private ArrayList<String> splitTransiciones(String hilera) {
+        
+        ArrayList<String> retorno = new ArrayList<>();
+        int size = hilera.length();
+        
+        int i = 0;
+        int j = 0;
+        while (i < size) 
+        {                
+            String s = "";
+            if(hilera.charAt(i) == '(')
+            {
+                i++;
+            }
+            while (i< size && hilera.charAt(i) != ')') 
+            {
+                s = s.concat(String.valueOf(hilera.charAt(i)));
+                i++;
+            }
+                retorno.add(s);
+                i++;
+                j++;
+        }
+        return retorno;
+    }
 }
